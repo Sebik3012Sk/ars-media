b2c14b35cac3d1cb08eaceebb08125da3fd11891
Sebik3012Sk <sebik-kucera@hotmail.com>
2024-09-14 16:32:57 +0200

    posts.sql upload

---
+++ posts.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Sob 14. zář 2024, 16:32
+-- Verze serveru: 10.4.32-MariaDB
+-- Verze PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáze: `ars-media`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_name` text NOT NULL,
+  `likes` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `id` int(11) NOT NULL,
+  `post_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Vypisuji data pro tabulku `posts`
+--
+
+INSERT INTO `posts` (`post_name`, `likes`, `description`, `id`, `post_content`) VALUES
+('Kurva Raketa', 28000, 'Kurva Suka Bliat Pojebala Ma tiktoka', 1, 'How do you live in this fucking world where do you destroyeed everything fucking president'),
+('Kit Song Tutorial', 20000, 'Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , Meow , You are dead bro', 2, 'No stres do přírody ');
+
+--
+-- Indexy pro exportované tabulky
+--
+
+--
+-- Indexy pro tabulku `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
